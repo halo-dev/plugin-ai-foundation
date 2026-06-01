2237dcb4bc7dee45133abb8bc26065a90e40fae5
LIlGG <1103069291@qq.com>
2026-06-01 12:07:15 +0800

    feat: add copy model id action

--- ui/src/views/components/AllModelListItem.vue
+++ ui/src/views/components/AllModelListItem.vue
@@ -20,6 +20,7 @@ import {
   VTag,
 } from '@halo-dev/components'
 import { useQueryClient } from '@tanstack/vue-query'
+import { useClipboard } from '@vueuse/core'
 import { computed, shallowRef } from 'vue'
 import { useRouter } from 'vue-router'
 import ModelEditingModal from './ModelEditingModal.vue'
@@ -30,6 +31,7 @@ const props = defineProps<{
 
 const queryClient = useQueryClient()
 const router = useRouter()
+const { copy, isSupported } = useClipboard()
 
 const editingModalVisible = shallowRef(false)
 
@@ -40,6 +42,26 @@ function openWorkbench() {
       model: props.model.metadata.name,
     },
   })
+}
+
+async function handleCopyModelId() {
+  const modelId = props.model.metadata.name
+  if (!modelId) {
+    Toast.error('模型 ID 为空')
+    return
+  }
+
+  if (!isSupported.value) {
+    Toast.error('当前浏览器不支持复制')
+    return
+  }
+
+  try {
+    await copy(modelId)
+    Toast.success('模型 ID 已复制')
+  } catch (error) {
+    Toast.error('模型 ID 复制失败: ' + (error as Error).message)
+  }
 }
 
 function handleDelete() {
@@ -89,6 +111,7 @@ const canTest = computed(() => isEnabledTestableModel(props.model))
     <template #dropdownItems>
       <VDropdownItem @click="editingModalVisible = true">编辑</VDropdownItem>
       <VDropdownItem v-if="canTest" @click="openWorkbench">测试</VDropdownItem>
+      <VDropdownItem @click="handleCopyModelId">复制模型 ID</VDropdownItem>
       <VDropdownDivider />
       <VDropdownItem type="danger" @click="handleDelete">删除</VDropdownItem>
     </template>
